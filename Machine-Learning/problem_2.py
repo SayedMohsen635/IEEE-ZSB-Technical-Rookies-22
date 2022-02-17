1193273eb927339a556af7aef64ae94a6d7355eb
Sayed <sayedmohsen635@gmail.com>
2022-02-17 03:57:57 +0200

    Ai_Task

--- Machine-Learning/problem_2.py
+++ Machine-Learning/problem_2.py
@@ -3,16 +3,18 @@ import matplotlib.pyplot as plt
 import os
 import cv2
 from tqdm import tqdm
+import pickle
+import random
 
-DATADIR = "C:/Users/moham/Downloads/kagglecatsanddogs_3367a.zip/PetImages"
+DATADIR = "C:/Users/sayed/Downloads/kagglecatsanddogs_3367a.zip/PetImages"
 
-CATEGORIES = ["Dog", "Cat"]
+CATEGORIES = ["Dog" , "Cat"]
 
 for category in CATEGORIES:
     path = os.path.join(DATADIR, category)
     for img in os.listdir(path):
-        img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
-        plt.imshow(img_array, cmap='gray')
+        img_array = cv2.imread(os.path.join(path, img) , cv2.IMREAD_GRAYSCALE)
+        plt.imshow(img_array , cmap = 'gray')
         plt.show()
         break
     break
@@ -22,7 +24,7 @@ print(img_array.shape)
 
 IMG_SIZE = 50
 new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-plt.imshow(new_array, cmap='gray')
+plt.imshow(new_array , cmap='gray')
 plt.show()
 
 training_data = []
@@ -30,21 +32,19 @@ training_data = []
 
 def create_training_data():
     for category in CATEGORIES:
-        path = os.path.join(DATADIR, category)
+        path = os.path.join(DATADIR , category)
         class_num = CATEGORIES.index(category)
         for img in os.listdir(path):
             try:
-                img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
-                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-                training_data.append([new_array, class_num])
+                img_array = cv2.imread(os.path.join(path, img) , cv2.IMREAD_GRAYSCALE)
+                new_array = cv2.resize(img_array , (IMG_SIZE, IMG_SIZE))
+                training_data.append([new_array , class_num])
             except Exception as e:
                 pass
 
 
 create_training_data()
 print(len(training_data))
-
-import random
 
 random.shuffle(training_data)
 
@@ -53,13 +53,12 @@ for sample in training_data[:10]:
 
 X = []
 y = []
-for features, label in training_data:
+for features , label in training_data:
     X.append(features)
     y.append(label)
 print(X[0].reshape(-1, IMG_SIZE, IMG_SIZE, 1))
 X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 
-import pickle
 
 pickle_out = open("X.pickle", "wb")
 pickle.dump(X, pickle_out)
--- Machine-Learning/problem_3.py
+++ Machine-Learning/problem_3.py
@@ -4,7 +4,6 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
-
 import pickle
 
 pickle_in = open("X.pickle", "rb")
